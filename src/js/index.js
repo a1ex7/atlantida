21a6b1af8db64495f9757ec4d61337e730133fed
a1ex7 <ta-lex@mail.ru>
2020-10-29 00:12:13 +0200

    [!] small fixes

--- src/js/index.js
+++ src/js/index.js
@@ -10,18 +10,18 @@ $(function() {
   /* Menu open */
   $('.js-menu-toggle').on('click', function() {
     $('.menu--mobile, .menu-icon').toggleClass('open');
-  })
+  });
 
   /* Smooth scroll */
   $( '.menu__link, .category__link' ).on('click', function() {
     /* close meu */
     $('.menu--mobile, .menu-icon').removeClass('open');
     /* go to section */
-    var targetId = $(this).attr('href');
-    var $targetSection = $(targetId);
+    const targetId = $(this).attr('href');
+    const $targetSection = $(targetId);
     if ($targetSection.length) {
-      var targetPosition = $targetSection.offset().top;
-      var headerHeight = $('.main-header').height();
+      const targetPosition = $targetSection.offset().top;
+      const headerHeight = $('.main-header').height();
       $('html, body').animate({
         scrollTop: targetPosition - headerHeight
       }, 400);
